351b998edd4b48d1f5cec7cfdf8f431f6e767845
Richard Kajaste <rkajaste@users.noreply.github.com>
2016-11-17 12:03:00 +0200

    added function

--- test.php
+++ test.php
@@ -1,7 +1,13 @@
 <?php
 
 	// hello
-	
+	function hello($hello){
+		echo "hello world ".$hello;
+		echo "hello world ".$hello;
+		echo "hello world ".$hello;
+		echo "hello world ".$hello;
+		
+	}
 	
 	
 	// welcome
